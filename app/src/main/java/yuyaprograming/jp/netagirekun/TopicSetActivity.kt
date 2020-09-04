76aa0e27cda80a55a4575f31a4f26b8c35c84aa8
yuyaprograming <ienjoyschoollife@gmail.com>
2020-09-04 20:16:31 +0900

    private fun showTimePickerDialog() {         val d = Date() // 現在時刻         val sdf = SimpleDateFormat("HH", Locale.getDefault())         val c =sdf.format(d)         val hourx : Int = Integer.parseInt(c)         val dd = Date() // 現在時刻         val sf = SimpleDateFormat("mm",Locale.getDefault())         val b =sf.format(dd)         val minutex : Int = Integer.parseInt(b)         val timePickerDialog = TimePickerDialog(             this,             TimePickerDialog.OnTimeSetListener { view, hour, minute ->             },             hourx+9, minutex, true)         timePickerDialog.show()     }
        デフォルトでは９時間遅かったので、+9をすることによって日本の時刻に合わせることに成功。

--- app/src/main/java/yuyaprograming/jp/netagirekun/TopicSetActivity.kt
+++ app/src/main/java/yuyaprograming/jp/netagirekun/TopicSetActivity.kt
@@ -10,8 +10,10 @@ import android.support.design.widget.Snackbar
 import android.util.Log
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_topic_set.*
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class TopicSetActivity : AppCompatActivity() {
     private lateinit var nRealm: Realm
@@ -20,19 +22,18 @@ class TopicSetActivity : AppCompatActivity() {
 
     private fun showTimePickerDialog() {
         val d = Date() // 現在時刻
-        val sdf = SimpleDateFormat("HH")
+        val sdf = SimpleDateFormat("HH", Locale.getDefault())
         val c =sdf.format(d)
         val hourx : Int = Integer.parseInt(c)
         val dd = Date() // 現在時刻
-        val sf = SimpleDateFormat("mm")
+        val sf = SimpleDateFormat("mm",Locale.getDefault())
         val b =sf.format(dd)
         val minutex : Int = Integer.parseInt(b)
         val timePickerDialog = TimePickerDialog(
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-                Log.d("UI_PARTS", "$hour:$minute")
             },
-            "hourx", "minutex", true)
+            hourx+9, minutex, true)
         timePickerDialog.show()
     }
 

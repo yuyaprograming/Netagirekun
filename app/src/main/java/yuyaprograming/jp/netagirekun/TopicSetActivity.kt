445e3494e5d90588a5f9d662a562026834e21e7a
yuyaprograming <ienjoyschoollife@gmail.com>
2020-09-06 21:38:25 +0900

    話題をintentで渡そうとしている。

--- app/src/main/java/yuyaprograming/jp/netagirekun/TopicSetActivity.kt
+++ app/src/main/java/yuyaprograming/jp/netagirekun/TopicSetActivity.kt
@@ -16,6 +16,10 @@ import java.util.*
 
 
 class TopicSetActivity : AppCompatActivity() {
+
+    private var mHour = 0
+    private var mMinute = 0
+
     private lateinit var nRealm: Realm
 
     private var mTopic: Topic? = null
@@ -32,6 +36,8 @@ class TopicSetActivity : AppCompatActivity() {
         val timePickerDialog = TimePickerDialog(
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                mHour = hour
+                mMinute = minute
             },
             hourx+9, minutex, true)
         timePickerDialog.show()
@@ -42,36 +48,57 @@ class TopicSetActivity : AppCompatActivity() {
         setContentView(R.layout.activity_topic_set)
         nRealm = Realm.getDefaultInstance()
 
+        val z = mHour + mMinute
+        val zz : Long = z.toLong()
+
         topic_set_button_above.setOnClickListener {
-            val taskRealmResults = nRealm.where(Topic::class.java).findAll()
-            if (taskRealmResults.size < 5) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("VALUE10", 10)
-                startActivity(intent)
-            } else {
-                showTimePickerDialog()
-                val b = mutableListOf<String?>()
-                for (i in 1..5) {
-                    var many = (0..taskRealmResults.size - 1).random()
-                    var a = taskRealmResults[many]?.contents
-                    while (b.contains(a)) {
-                        many = (0..taskRealmResults.size - 1).random()
-                        a = taskRealmResults[many]?.contents
+            if (zz == null) {
+                topic_set_button_above.text = "ランダムで話題をセットする"
+                val taskRealmResults = nRealm.where(Topic::class.java).findAll()
+                if (taskRealmResults.size < 5) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("VALUE10", 10)
+                    startActivity(intent)
+                } else {
+                    showTimePickerDialog()
+                    val b = mutableListOf<String?>()
+                    for (i in 1..5) {
+                        var many = (0..taskRealmResults.size - 1).random()
+                        var a = taskRealmResults[many]?.contents
+                        while (b.contains(a)) {
+                            many = (0..taskRealmResults.size - 1).random()
+                            a = taskRealmResults[many]?.contents
+                        }
+                        b.add(a)
                     }
-                    b.add(a)
+                    val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                    resultIntent.putExtra("topic_x", "b")
+                    val resultPendingIntent = PendingIntent.getBroadcast(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+
+                    val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                    alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, zz, resultPendingIntent)
+                    Log.d("kotlintest", b.toString())
                 }
+            } else {
+                topic_set_button_above.text = "設定した話題を解除する"
                 val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
-                resultIntent.putExtra("topic_x", "b")
                 val resultPendingIntent = PendingIntent.getBroadcast(
-                    this,
+                    this@TopicSetActivity,
                     0,
                     resultIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT
                 )
 
                 val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, resultPendingIntent)
-                Log.d("kotlintest", b.toString())
+                alarmManager.cancel(resultPendingIntent)
+                Snackbar.make(findViewById(R.id.activity_topic_set), "セットした話題が削除されました", Snackbar.LENGTH_LONG)
+                    .setAction("閉じる"){
+                    }.show()
             }
         }
 
